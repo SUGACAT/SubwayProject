e71ca99a2e8b0ecffc3e14265c4cc853069d82c6
Elza <xpfkqmffor@gmail.com>
2023-06-29 16:46:11 +0900

    zzzzzzzzzz

---
+++ Assets/Script/Game/MonsterSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject catMonster_Obj, ratMonster_Obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnMonster(Vector3 Position)
+    {
+        
+    }
+
+    public void SpawnMonsterByPosition(Transform[] positions)
+    {
+        
+    }
+}
